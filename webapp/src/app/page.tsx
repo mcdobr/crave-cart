7a3ea3e041f2f77e06de8f8a72fb0a7647c4a481
Mircea Dobreanu <mircea@dobreanu.net>
2024-08-16 16:07:07 +0300

    Create a simple Profile component using mocked data

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Profile from "@/app/profile/profile";
 
 export default function Home() {
-
-  const restaurants = [
-      {
-        name: 'Restaurant 1',
-      },
+    const restaurants = [
         {
-        name: 'Restaurant 2'
-       },
-  ];
+            id: 1,
+            name: 'Restaurant 1',
+        },
+        {
+            id: 2,
+            name: 'Restaurant 2'
+        },
+    ];
 
-  const restaurantListItems = restaurants.map(restaurant => <li key={restaurant.name}>{restaurant.name}</li>);
+    const restaurantListItems = restaurants.map(restaurant => <li key={restaurant.name}>{restaurant.name}</li>);
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <ul>{restaurantListItems}</ul>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Profile/>
+            <ul>{restaurantListItems}</ul>
+        </main>
+    );
 }
---
+++ webapp/src/app/profile/profile.tsx
@@ -0,0 +1,22 @@
+const user = {
+    name: 'Mircea',
+    imageUrl: 'https://i.imgur.com/8uq1yHb.jpeg',
+    imageSize: 90,
+};
+
+export default function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize,
+                }}
+            />
+        </>
+        );
+}
